88579982ac4a4038d42f563413b273b56f181470
AndriiNychaiuk <anichajuk@gmail.com>
2022-09-21 13:05:23 +0300

    data base created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
+import { CatalogList } from './components/CatalogList/CatalogList';
+import { Categories } from './components/Categories/Categories';
+import { Product } from './types/Product';
+import clothes from './api/clothes.json'
 
-function App() {
+
+const App: React.FC = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [categories, setCategories] = useState<string[]>([]);
+
+  useEffect(() => {
+    setProducts(clothes)
+
+    const arr: string[] = [];
+
+    for (const cloth of clothes) {
+      if (!arr.includes(cloth.category)) {
+        arr.push(cloth.category);
+      }
+    }
+
+    setCategories(arr)
+  }, []);
+
+  console.log(products);  
+  console.log(categories);
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App__header">
+        <div className="App__container">
+          <h2 className="App__title">Shop</h2>
+        </div>
       </header>
+
+      <main className="App__main">
+        <div className="App__container">
+          <div className="App__flex">
+            <Categories categories={categories} />
+            <CatalogList products={products} />
+          </div>
+          
+        </div>
+      </main>
+
+      <footer className="App__footer">
+        <div className="App__container">
+          <h2 className="App__title">Shop</h2>
+        </div>
+      </footer>
     </div>
   );
 }
